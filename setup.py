59dd284dc83b938fb6918cbe51d9b7773884d65c
mariojager <marios_katsak@outlook.com>
2016-03-06 18:46:00 +0000

    making the setup config file

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
-   
+from setuptools import setup, find_packages
+
+setup(name='dragila',
+      version='0.1',
+      description='',
+      url='https://github.com/mariosgohan/dragila',
+      author='mariosgohan',
+      author_email='marios_katsak@outlook.com',
+      license='LICENSE',
+      packages=find_packages())
